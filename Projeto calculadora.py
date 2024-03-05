ad44c218b2b9b00c57421d71b2cf37969dad2b2b
RodrigoF-Braga <161756897+RodrigoF-Braga@users.noreply.github.com>
2024-03-05 16:42:35 -0300

    Update-01-Projeto calculadora.py
    Modificada a estrutura de calculos, agora usando funções

--- Projeto calculadora.py
+++ Projeto calculadora.py
@@ -1,45 +1,76 @@
+import matplotlib.pyplot as plt
 while True:
-    print(f"========== Calculadora =========")
-    print("1. Soma (+)")
-    print("2. Subtração (-)")
-    print("3. Multiplicação (*)")
-    print("4. Divisão (//)")
-    print("5. Potenciação (**)")
-    print("6. Sair")
+    texto = '''
+    ========== Calculadora =========
+    1. Soma (+)
+    2. Subtração (-)
+    3. Multiplicação (*)
+    4. Divisão (//)
+    5. Potenciação (**)
+    6. Fatorial (!)
+    7. Sair
+    '''
+    print(texto)
 
-    resposta = int(input("Selecione uma das opções: "))
+    resposta = input("Selecione uma das opções: ")
+    if resposta in ('6'):
+        num1 = int(input("Insira o numero: "))
+        def Fatorial(num1):
+            fatorial = 1
+            for i in range (num1, 1, -1):
+                fatorial *= i
+            return fatorial
+        print(f"O resultado é {Fatorial(num1)}")
+    elif resposta in ('1','2','3','4','5'):
+        num1 = float(input("Insira o primeiro numero: "))
+        num2 = float(input("Insira o segundo numero: "))
+
+    def Soma(num1,num2):
+        return num1 + num2
+
+    def Subtração(num1,num2):
+        return num1 - num2
+
+    def Multiplicação(num1,num2):
+        return num1 * num2
+
+    def Divisão(num1,num2):
+        return num1 // num2
+    def Potenciação(num1,num2):
+        return num1 ** num2
+
     if resposta == 1:
-        num1 = float(input("Insira o primeiro numero: "))
-        num2 = float(input("Insira o segundo numero: "))
-        resultado_1 = num1 + num2
-        print(f"O resultado é {resultado_1}")
+        print(f"O resultado é {Soma(num1,num2)}")
+
     elif resposta == 2:
-        num1 = float(input("Insira o primeiro numero: "))
-        num2 = float(input("Insira o segundo numero: "))
-        resultado_2 = num1 + num2
-        print(f"O resultado é {resultado_2}")
+        print(f"O resultado é {Subtração(num1,num2)}")
+
     elif resposta == 3:
-        num1 = float(input("Insira o primeiro numero: "))
-        num2 = float(input("Insira o segundo numero: "))
-        resultado_3 = num1 * num2
-        print(f"O resultado é {resultado_3}")
+        print(f"O resultado é {Multiplicação(num1,num2)}")
+
     elif resposta == 4:
-        num1 = float(input("Insira o primeiro numero: "))
-        num2 = float(input("Insira o segundo numero: "))
-        resultado_4 = num1 // num2
-        print(f"O resultado é {resultado_4}")
+        print(f"O resultado é {Divisão(num1,num2)}")
+
     elif resposta == 5:
-        num1 = float(input("Insira o primeiro numero: "))
-        num2 = float(input("Insira o segundo numero: "))
-        resultado_5 = num1 ** num2
-        print(f"O resultado é {resultado_5}")
-    elif resposta == 6:
+        print(f"O resultado é {Potenciação(num1,num2)}")
+
+    elif resposta == 7:
         break
+
     else:
         print("Insira uma resposta valida")
     print("Selecione 1 para continuar e 2 para parar")
     resposta_2 = int(input(""))
     if resposta_2 == 2:
+        texto = ''''
+        Obrigado por usar
+        ,,,,,,,,,,,,,,,,,,,,__
+        ,,,,,,,,,,,,,,,,,,,/ *_) . -♥-♥-♥-♥-♥-♥-
+             ________,—-,_/,,/ ,
+        ,,,,/,,,,,,,,,,,,,,/
+          _/.....…(…|.(…|)
+         ./__....-|_|–|_|
+        '''
         break
     elif resposta_2 != 1 and 2:
         print("Insira uma resposta valida")
